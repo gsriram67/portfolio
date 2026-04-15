3fda5c463f6a6d5a55f5e9462138a5533c66bf21
gsriram67 <gsriram@ucsd.edu>
2026-04-14 17:40:41 -0700

    lab 3 changes

---
+++ global.js
@@ -0,0 +1,78 @@
+console.log('IT’S ALIVE!');
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'resume/', title: 'Resume' },
+    { url: 'contact/', title: 'Contact' },
+    { url: 'https://github.com/gsriram67', title: 'GitHub' }
+    // add the rest of your pages here
+];
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+for (let p of pages) {
+    let url = p.url;
+    let title = p.title;
+
+    const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+        ? "/"                  // Local server
+        : "/portfolio/";         // GitHub Pages repo name
+
+    url = !url.startsWith('http') ? BASE_PATH + url : url;
+
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+    } else if (a.host != location.host) {
+        a.target = '_blank';
+    }
+    nav.append(a);
+}
+
+document.body.insertAdjacentHTML(
+    'afterbegin',
+    `
+	<label class="color-scheme">
+		Theme:
+		<select>
+	        <option value="light dark">Default</option>
+            <option value="light">Light</option>
+            <option value="dark">Dark</option>
+		</select>
+	</label>`,
+);
+
+function setColorScheme(scheme) {
+    document.documentElement.style.setProperty('color-scheme', scheme);
+}
+
+if ('colorScheme' in localStorage) {
+    setColorScheme(localStorage.colorScheme);
+}
+
+let select = document.querySelector("select")
+
+select?.addEventListener('input', function(event) {
+    setColorScheme(event.target.value);
+    localStorage.colorScheme = event.target.value;
+});
+
+
+let form = document.querySelector("form")
+form?.addEventListener('submit', function(event) {
+    event.preventDefault()
+    console.log("form submitted");
+    let baseUrl = form.action;
+    let params = [];
+    for (let [name, value] of new FormData(form)) {
+        params.push(name + "=" + encodeURIComponent(value));
+    }
+    let finalUrl = baseUrl + "?" + params.join("&");
+    console.log("Navigating to:", finalUrl);
+    location.href = finalUrl;
+});
+
