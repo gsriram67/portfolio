dd684ab406479cefa0efabd14ea7a68ff39b14b6
gsriram67 <gsriram@ucsd.edu>
2026-05-06 16:45:59 -0700

    install statistics package

--- global.js
+++ global.js
@@ -5,6 +5,7 @@ let pages = [
     { url: 'projects/', title: 'Projects' },
     { url: 'resume/', title: 'Resume' },
     { url: 'contact/', title: 'Contact' },
+    { url: 'meta/', title: 'Meta' },
     { url: 'https://github.com/gsriram67', title: 'GitHub' }
     // add the rest of your pages here
 ];
